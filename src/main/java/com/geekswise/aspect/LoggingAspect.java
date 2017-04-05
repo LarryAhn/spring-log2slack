cca9eba26917c28ea2cec7e29f0c4fb65497299a
Ahn <lawrenceahn27@gmail.com>
2017-04-05 15:07:42 +0900

    use annotation @Slf4j

--- src/main/java/com/geekswise/aspect/LoggingAspect.java
+++ src/main/java/com/geekswise/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.geekswise.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -14,24 +15,23 @@ import java.util.Arrays;
  */
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Around("@annotation(Logging)")
     public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
         /** 대상 method 실행 전 실행 구역 **/
-        logger.info("### Enter : {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+        log.info("### Enter : {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
                 joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
         /** 대상 method 실행 전 실행 구역 종료 **/
         try {
             Object result = joinPoint.proceed();
             /** 대상 method 실행 후 끼어들어 실행할 로직을 작성, 리턴 값을 가공할 수 있다. **/
-            logger.info("### Exit : {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+            log.info("### Exit : {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
                     joinPoint.getSignature().getName(), result);
             return result;
         } catch (IllegalArgumentException e) {
-            logger.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
                     joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
             throw e;
         }
